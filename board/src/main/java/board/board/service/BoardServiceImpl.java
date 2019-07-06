bf0a1501427da3709b0c83e54bc61287899394d5
KimHyunSeo <hyseokim@gmail.com>
2019-07-07 02:06:16 +0900

    second update

--- board/src/main/java/board/board/service/BoardServiceImpl.java
+++ board/src/main/java/board/board/service/BoardServiceImpl.java
@@ -18,5 +18,17 @@ public class BoardServiceImpl implements BoardService{
 	public List<BoardDto> selectBoardList() throws Exception {
 		return boardMapper.selectBoardList();
 	}
-
+	
+	@Override
+	public void insertBoard(BoardDto board) throws Exception {
+		boardMapper.insertBoard(board);	
+	}
+	
+	@Override
+	public BoardDto selectBoardDetail(int boardIdx) throws Exception{
+		boardMapper.updateHitCount(boardIdx);
+		
+		BoardDto board = boardMapper.selectBoardDetail(boardIdx);
+		return board;
+	}
 }
