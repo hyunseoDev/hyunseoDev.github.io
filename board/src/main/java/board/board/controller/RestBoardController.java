c540290e12434227e6e113fab3c732838abff245
hyunseoDev <hyseokim@gmail.com>
2020-01-11 15:21:19 +0900

    test commit

--- board/src/main/java/board/board/controller/RestBoardController.java
+++ board/src/main/java/board/board/controller/RestBoardController.java
@@ -7,12 +7,13 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,54 +23,55 @@ import board.board.dto.BoardFileDto;
 import board.board.service.BoardService;
 
 @Controller
-public class BoardController {
-	private Logger log = LoggerFactory.getLogger(this.getClass());
+public class RestBoardController {
+
 	@Autowired
 	private BoardService boardService;
 	
-	@RequestMapping("/board/openBoardList.do")
+	@RequestMapping(value="/board", method=RequestMethod.GET)
 	public ModelAndView openBoardList() throws Exception{
-		log.debug("openBoardList");
-		ModelAndView mv = new ModelAndView("/board/boardLIst");
+		ModelAndView mv = new ModelAndView("/board/restBoardList");
 		
 		List<BoardDto> list = boardService.selectBoardList();
 		mv.addObject("list", list);
 		
 		return mv;
 	}
-	@RequestMapping("/board/openBoardWrite.do")
+	
+	@RequestMapping(value="/board/write", method=RequestMethod.GET)
 	public String openBoardWrite() throws Exception{
-		return "/board/boardWrite";
+		return "/board/restBoardWrite";
 	}
 	
-	@RequestMapping("/board/insertBoard.do")
+	@RequestMapping(value="/board/write", method=RequestMethod.POST)
 	public String insertBoard(BoardDto board, MultipartHttpServletRequest multipartHttpServletRequest) throws Exception{
 		boardService.insertBoard(board, multipartHttpServletRequest);
-		return "redirect:/board/openBoardList.do";
+		return "redirect:/board";
 	}
 	
-	@RequestMapping("/board/openBoardDetail.do")
-	public ModelAndView openBoardDetail(@RequestParam int boardIdx) throws Exception{
-		ModelAndView mv = new ModelAndView("/board/boardDetail");
+	@RequestMapping(value="/board/{boardIdx}", method=RequestMethod.GET)
+	public ModelAndView openBoardDetail(@PathVariable("boardIdx") int boardIdx, ModelMap model) throws Exception{
+		ModelAndView mv = new ModelAndView("/board/restBoardDetail");
 		
 		BoardDto board = boardService.selectBoardDetail(boardIdx);
 		mv.addObject("board", board);
+		
 		return mv;
 	}
 	
-	@RequestMapping("/board/updateBoard.do")
+	@RequestMapping(value="/board/{boardIdx}", method=RequestMethod.PUT)
 	public String updateBoard(BoardDto board) throws Exception{
 		boardService.updateBoard(board);
-		return "redirect:/board/openBoardList.do";
+		return "redirect:/board";
 	}
 	
-	@RequestMapping("/board/deleteBoard.do")
-	public String deleteBoard(int boardIdx) throws Exception{
+	@RequestMapping(value="/board/{boardIdx}", method=RequestMethod.DELETE)
+	public String deleteBoard(@PathVariable("boardIdx") int boardIdx) throws Exception{
 		boardService.deleteBoard(boardIdx);
-		return "redirect:/board/openBoardList.do";
+		return "redirect:/board";
 	}
 	
-	@RequestMapping("/board/downloadBoardFile.do")
+	@RequestMapping(value="/board/file", method=RequestMethod.GET)
 	public void downloadBoardFile(@RequestParam int idx, @RequestParam int boardIdx, HttpServletResponse response) throws Exception{
 		BoardFileDto boardFile = boardService.selectBoardFileInformation(idx, boardIdx);
 		if(ObjectUtils.isEmpty(boardFile) == false) {
@@ -79,12 +81,12 @@ public class BoardController {
 			
 			response.setContentType("application/octet-stream");
 			response.setContentLength(files.length);
-			response.setHeader("Content-Disposition", "attachment; fileName=\"" + URLEncoder.encode(fileName, "UTF-8") + "\";");
+			response.setHeader("Content-Disposition", "attachment; fileName=\"" + URLEncoder.encode(fileName,"UTF-8") + "\";");
 			response.setHeader("Content-Transfer-Encoding", "binary");
 			
 			response.getOutputStream().write(files);
 			response.getOutputStream().flush();
 			response.getOutputStream().close();
-		}	
-	}
+		}
+	}	
 }
